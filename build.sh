591f5ef4a4df25701be5ebdf15bd98d9e266ea00
Sanni Chauhan <chauhansanni94@gmail.com>
2026-02-09 21:33:17 +0530

    updated build

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver
+gunicorn HRM_lite.wsgi:application --bind 0.0.0.0:8000
