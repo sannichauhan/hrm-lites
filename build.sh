98985998060905db07d12305323f311bd19da62a
Sanni Chauhan <chauhansanni94@gmail.com>
2026-02-09 19:16:58 +0530

    port change

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8107
