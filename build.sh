847eaa6779da37311b68dbb7ef9bd339313a42c8
Sanni Chauhan <chauhansanni94@gmail.com>
2026-02-09 21:24:10 +0530

    updated port number

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver
